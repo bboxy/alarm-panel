53c4beb158b56132baaf6fb762ba25c03f9be0e5
Tobias Bindhammer <tobias.bindhammer@uni-ulm.de>
2016-07-14 08:15:00 +0200

    fix timestamps and expring of fms-statuses

--- fms.pl
+++ fms.pl
@@ -18,11 +18,13 @@ local $SIG{ALRM} = sub {
 	print "timeout\n";
 	# Suche in unseren Fahrzeugen nach alten Einträgen
 	for (my $fzg = 0; $fzg <= $#{$fuhrpark->{fuhrpark}->{fahrzeug}}; $fzg++) {
-		if (time() > $fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{timestamp} + 6 * 60) {
-                        print "expiring entry\n";
-			$fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{status} = 2;
-			$fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{timestamp} = time();
-			$xml->XMLout($fuhrpark);
+		if (time() > $fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{timestamp} + 6 * 60 * 60) {
+			if ($fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{status} != 2) {
+				print "expiring entry\n";
+				$fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{status} = 2;
+				$fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{timestamp} = time();
+				$xml->XMLout($fuhrpark);
+			}
 		}
 	}
 };
@@ -47,17 +49,16 @@ while(<>) {
 			# Status ausschneiden
 			$status = substr($fms,3,1);
 
-			print "$fms $id $status\n";
-
 			# Suche in unseren Fahrzeugen nach ID
 			for (my $fzg = 0; $fzg <= $#{$fuhrpark->{fuhrpark}->{fahrzeug}}; $fzg++) {
 				#printf $fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{id} . "\n";
 				if ($id eq $fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{id}) {
 					if ($status > 0 && $status < 7 && $status != 5) {
+						# set new time stamp first to aoid expiring and reset to status 2
+						$fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{timestamp} = time();
 						print ("FMS: $fms\n");
 						print "Found in XML, updating status for $id to $status\n";
 						$fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{status} = $status;
-						$fuhrpark->{fuhrpark}->{fahrzeug}->[$fzg]->{timestamp} = time();
 						$xml->XMLout($fuhrpark);
 					}
 				}
